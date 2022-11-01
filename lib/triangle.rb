eb50c3f576b79256895854b5df1a2de82b5a02c9
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-11-01 12:22:28 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+  attr_accessor :base, :height, :length
+  def initialize(length1,length2,length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+
+  end
+
+  def kind
+    if [@length1,@length2,@length3].any? {|length| length<=0 }
+    raise TriangleError
+    end
+
+    sides= [@length1,@length2,@length3].sort
+
+    unless sides[0]+sides[1] > sides[2]
+      raise TriangleError   
+    end
+
+    case [@length1,@length2,@length3].uniq.size
+    when 1 then :equilateral
+      
+    when 2 then :isosceles
+      
+    else :scalene
+      
+    end
+  end
+
+  class TriangleError < StandardError
+    def method_name
+      "You must give the right measured sides"
+    end
+  end
 end
